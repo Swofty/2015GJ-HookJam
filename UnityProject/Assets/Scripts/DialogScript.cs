81f765755cfb1c012119ee7f445e4911c1a0f6f3
Roger Liu <rogliu26@gmail.com>
2015-01-24 13:58:35 -0500

    .... no idea actually. Cleaned some code and attempted dialog boxes

---
+++ UnityProject/Assets/Scripts/DialogScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void OnGUI()
+    {
+        GUI.Box(new Rect(5, 5, Screen.width - 5, Screen.height - 5), "My box");
+    }
+}
