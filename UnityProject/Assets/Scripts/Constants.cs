4b909f2782309e51ba335ffa8a3c57d831b8008a
Roger Liu <rogliu26@gmail.com>
2015-01-24 11:36:52 -0500

    Made an additional method in constants that allows translation from a vector to a direction

--- UnityProject/Assets/Scripts/Constants.cs
+++ UnityProject/Assets/Scripts/Constants.cs
@@ -5,7 +5,7 @@ public static class Constants{
 
     public enum Dir { N, E, S, W };
 
-    public static Vector2 getDirectionVector(Dir direction)
+    public static Vector2 getVectorFromDirection(Dir direction)
     {
         switch (direction)
         {
@@ -15,5 +15,28 @@ public static class Constants{
             case Dir.W: return new Vector2(-1.0f, 0.0f);
         }
         return new Vector2(0.0f, 0.0f);
+    }
+
+    public static Dir getDirectionFromVector(Vector3 direction_vector)
+    {
+        if (direction_vector.y >= direction_vector.x && direction_vector.y >= 0)
+        {
+            return Dir.N;
+        }
+        else if (direction_vector.y >= direction_vector.x && direction_vector.y < 0)
+        {
+            return Dir.S;
+        }
+        if (direction_vector.x > direction_vector.y && direction_vector.x >= 0)
+        {
+            return Dir.E;
+        }
+        else if (direction_vector.x > direction_vector.y && direction_vector.x < 0)
+        {
+            return Dir.W;
+        }
+
+        //The above cases should be sufficient
+        return Dir.N;
     }
 }
--- UnityProject/Assets/Scripts/EnemyScript.cs
+++ UnityProject/Assets/Scripts/EnemyScript.cs
@@ -1,4 +1,2 @@
-﻿using UnityEngine;using System.Collections;public class EnemyScript : MonoBehaviour {    public float speed;
-    public Constants.Dir direction;	// Use this for initialization	void Start () {
-        direction = Constants.Dir.S;	}		// Update is called once per frame	void Update () {	    	}    void FixedUpdate()    {
-        rigidbody2D.velocity = Constants.getDirectionVector(direction) * speed;    }}
+﻿using UnityEngine;using System.Collections;public class EnemyScript : MonoBehaviour {    public float speed;    public Constants.Dir direction;	// Use this for initialization	void Start () {        direction = Constants.Dir.S;	}		// Update is called once per frame	void Update () {	}    void OnTriggerEnter(Collider other)    {
+        Vector3 direction = other.transform.position - transform.position;    }    void FixedUpdate()    {        rigidbody2D.velocity = Constants.getVectorFromDirection(direction) * speed;    }}
