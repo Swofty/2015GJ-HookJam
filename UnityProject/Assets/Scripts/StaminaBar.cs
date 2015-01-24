f08312153e7cc5afff717b3b1a0f3d8d2475394a
jmiooo <leet2013@gmail.com>
2015-01-24 12:09:04 -0500

    Stamina bar.

---
+++ UnityProject/Assets/Scripts/StaminaBar.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StaminaBar : MonoBehaviour {
+
+	public float maximum = 1f;
+	public float length = 1f;
+	public float chargeModifier = 0.2f;
+
+	public float delay = 3f;
+	public float timer = 0f;
+
+	public float spaceCost = 0.1f;
+
+	void Update() {
+		float timePassed = Time.deltaTime;
+		float chargeTime = 0f;
+
+		if (Input.GetKeyDown(KeyCode.Space) && length >= spaceCost * 0.99999f) {
+			length = (length - spaceCost >= 0f) ? length - spaceCost : 0f;
+			timer = delay;
+		}
+		else {
+			if (timePassed > timer) {
+				chargeTime = timePassed - timer;
+				timer = 0f;
+			}
+			else {
+				timer -= timePassed;
+			}
+
+			length = ((length + chargeTime * chargeModifier > maximum)
+			          ? maximum
+			          : length + chargeTime * chargeModifier);
+		}
+		transform.localScale = new Vector3(length * 0.5f, 0.1f, 1f);
+		transform.position = new Vector3((length - maximum) * 7.2f, 0, 0);
+		Debug.Log(length);
+	}
+}
